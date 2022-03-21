13ec19d69a8198b57b5fd1b2270a6000c27bdba6
shedtepherd <>
2022-03-20 20:06:23 -0700

    add pseudo code

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ const button = document.getElementById("test")
 button.addEventListener("click", test)
 
 function test() {
+    // Create array of domains based on bookmarks
+    // Create folders named after array 
+    // Move bookmarks based on domain name to folders name after domains
     const parentTitle = 'Extension bookmarks'
     chrome.bookmarks.create({ 'title': parentTitle });
     document.getElementById("test").innerHTML = "All done!";
